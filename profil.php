a163bb5ee3d968bd8fe64abcbe8ca2bd850c2b79
Andrewbcq <120384050+Andrewbcq@users.noreply.github.com>
2023-03-29 18:26:03 +0200

    Page de profil de l'utilisateur

---
+++ profil.php
@@ -0,0 +1,90 @@
+
+<!DOCTYPE html> 
+<html> 
+<head> 
+    <meta charset="utf-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <title>Sa cook</title> 
+    <style type="text/css">
+    form {
+    display: inline-block;
+    }
+    </style>
+    <script>
+        function confirmation(){
+            if (confirm("Êtes-vous sûr de vouloir supprimer votre profil ? Cette action est irréversible.")) {
+            document.getElementById('confirm').value = 'yes';
+            document.getElementsByName('suprofil')[0].value = 'Valider la suppresion';
+            } 
+            else {
+                document.getElementById('confirm').value = 'no';
+            }
+        }
+    </script>
+</head> 
+<body> 
+    <form action="index.php" method="post">
+        <input type="submit" value="Aller à l'accueil">
+    </form>
+    <div>
+        <?php
+        session_start();
+        if ($_SESSION["droit"] != -1) {
+            include 'database.php';
+            global $db;
+            $login = $_SESSION['Login'];
+            $c = $db->prepare("SELECT Pseudo FROM Utilisateur WHERE Login = :Login");
+            $c->execute(['Login' => $login]);
+            $resultat = $c->fetch(PDO::FETCH_ASSOC);
+            echo $resultat['Pseudo'],'<br>';
+            $r=$db->prepare("SELECT * FROM Commentaire WHERE IdAuteur = :IdAuteur");
+            $r->execute(['IdAuteur'=>$_SESSION['id']]);
+            $result = $r->fetchAll(PDO::FETCH_ASSOC);        
+            foreach($result as $comm){
+                echo $comm['Commentaire'],'
+                <form method="post">
+                    <button type="submit" name= "suprComm" value=',$comm['IdCommentaire'],'>suprimier le commentaire</button>
+                </form><br><br>';
+            }
+        }
+        else {
+            header("Location:index.php");
+        }
+        ?>
+    </div>
+    <form id="supresion" method="post">
+        <input type="hidden" name="confirm" id="confirm" value="">
+        <input type="submit" name="suprofil" value="Supprimer le compte">
+    </form>
+
+</body> 
+<?php
+if ($_SESSION["droit"] != -1) {
+        if (isset($_POST['suprofil'])) {
+            if (isset($_POST['confirm']) && $_POST['confirm'] == 'yes') {
+                $stmt = $db->prepare("DELETE FROM Commentaire WHERE IdAuteur = :auteur");
+                $stmt->execute([':auteur' =>$_SESSION['id']]);
+                $stmt = $db->prepare("DELETE Commentaire FROM Recette JOIN Commentaire ON IdRecette=Recette_com WHERE IdCréateur= :createur;");
+                                 
+                $stmt->execute([':createur' =>$_SESSION['id']]);
+                $stmt = $db->prepare("DELETE FROM Recette WHERE IdCréateur = :Idcomm");
+                $stmt->execute([':Idcomm' =>$_SESSION['id']]);
+                $stmt = $db->prepare("DELETE FROM Utilisateur WHERE Login = :Login");
+                $stmt->execute([':Login' => $login]);
+                header("Location:deconnexion.php");
+                exit();
+            }
+             echo'<script>confirmation()</script>';
+        }    
+        if (isset($_POST['suprComm'])) {
+            $commasupr = $_POST['suprComm'];
+            $stmt = $db->prepare("DELETE FROM Commentaire WHERE IdCommentaire = :Idcomm");
+            $stmt->execute([':Idcomm' =>$commasupr]);
+            header("Location:profil.php");
+        }
+        }
+else {
+    header("Location:index.php");
+}
+?>
+</html>
