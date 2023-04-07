be5d4b5c7be09e58a7d217dc72e832c71645a4a2
Andrewbcq <120384050+Andrewbcq@users.noreply.github.com>
2023-04-07 16:00:17 +0200

    selection par date commentaire

--- profil.php
+++ profil.php
@@ -9,6 +9,9 @@
     form {
     display: inline-block;
     }
+    body{
+        background-color: grey;
+    }
     </style>
     <script>
         function confirmation(){
@@ -36,43 +39,79 @@
             $c = $db->prepare("SELECT Photo,Pseudo,IdUtilisateur FROM Utilisateur WHERE Login = :Login");
             $c->execute(['Login' => $login,]);
             $resultat = $c->fetch(PDO::FETCH_ASSOC);
-            echo "<img src=/Images/Pdp/",$_SESSION['id'],".jpg width='100px' height='100px'> </img>";
+            $taille_image=filesize("Images/Pdp/" . $_SESSION['id'] . ".jpg");
+
+            if($taille_image>50){
+                echo "<img src=/Images/Pdp/",$_SESSION['id'],".jpg width='100px' height='100px'> </img>";
+            }
+            else{echo "<img src=/Images/Pdp/userblanc.png width='100px' height='100px'> </img>";}
             echo $resultat['Pseudo'],'
                 <form method="post" enctype="multipart/form-data">
                     <label for="file">Sélectionnez Votre nouvelle photo de profil:</label>
                     <input type="file" id="file" name="file">
                     <input type="submit" value="Télécharger">
+
                 </form>';
-                if(isset($_FILES['file'])) {
-                    $file_name = $_FILES['file']['name'];
-                    $file_tmp = $_FILES['file']['tmp_name'];
-                    $file_type = $_FILES['file']['type'];
-                    $file_size = $_FILES['file']['size'];
-                    $target_dir = "Images/Pdp/"; // dossier de destination
-                    $target_file = $target_dir . basename($file_name);
-                  
-                    // Vérifier le type de fichier
-                    $allowed_types = array('image/jpeg', 'image/png');
-                    if(!in_array($file_type, $allowed_types)) {
-                      echo "Erreur: Seules les images JPEG et PNG sont autorisées.";
-                    }
-                    // Vérifier la taille du fichier
-                    else if($file_size > 5000000) { // 5 Mo maximum
-                      echo "Erreur: La taille du fichier doit être inférieure à 5 Mo.";
+            if(isset($_FILES['file'])) {
+                $file_name = $_FILES['file']['name'];
+                $file_tmp = $_FILES['file']['tmp_name'];
+                $file_type = $_FILES['file']['type'];
+                $file_size = $_FILES['file']['size'];
+                $target_dir = "Images/Pdp/"; // dossier de destination
+                $target_file = $target_dir . basename($file_name);
+                
+                // Vérifier le type de fichier
+                $allowed_types = array('image/jpeg', 'image/png');
+                if(!in_array($file_type, $allowed_types)) {
+                    echo "Erreur: Seules les images JPEG et PNG sont autorisées.";
+                }
+                // Vérifier la taille du fichier
+                else if($file_size > 5000000) { // 5 Mo maximum
+                    echo "Erreur: La taille du fichier doit être inférieure à 5 Mo.";
+                }
+                else {
+                    if(move_uploaded_file($file_tmp, $target_file)) {
+                    echo "Le fichier a été téléchargé avec succès.";
+                    rename($target_file,$target_dir . $resultat['IdUtilisateur'] . ".jpg");
+                    
                     }
                     else {
-                      if(move_uploaded_file($file_tmp, $target_file)) {
-                        echo "Le fichier a été téléchargé avec succès.";
-                      rename($target_file,$target_dir . $resultat['IdUtilisateur'] . ".jpg");
-                      }
-                      else {
-                        echo "Erreur lors du téléchargement du fichier.";
-                      }
+                    echo "Erreur lors du téléchargement du fichier.";
                     }
-                  }
-            $r=$db->prepare("SELECT * FROM Commentaire WHERE IdAuteur = :IdAuteur");
-            $r->execute(['IdAuteur'=>$_SESSION['id']]);
-            $result = $r->fetchAll(PDO::FETCH_ASSOC);        
+                }
+            }
+            
+            ?>
+            <div id="comment_select">
+                <h2>Commentaires</h2>
+                <form method="post">
+                    <select name="tri" id="tri">
+                        <option value="avant">Avant</option>
+                        <option value="aprés">Aprés</option>
+                    <input type="date" id="datePicker" name="datePicker">
+                    <input type="submit" name="submit" value="Aplliquer le tri"/>
+                </form>
+                <script>document.getElementById('datePicker').valueAsDate = new Date();</script>
+            </div>
+            <?php
+            if (!isset($_POST['tri'])) {
+                $_POST["tri"] = "vide";
+            }
+            if($_POST['tri']=='avant') {
+                    $sql = $db->prepare("SELECT Commentaire,Note,Date FROM Commentaire
+                    WHERE IdAuteur=:Recette_com
+                    AND Date<:dateselect 
+                    ORDER BY Date DESC");
+            }
+            else{
+                    $sql = $db->prepare("SELECT Commentaire,Note,Date FROM Commentaire
+                    WHERE IdAuteur=:Recette_com
+                    AND Date>:dateselect
+                    ORDER BY Date");
+            }
+            $sql->execute(['Recette_com'=>$_SESSION['id'],
+                           'dateselect'=>$_POST['datePicker']]);
+            $result = $sql->fetchAll(PDO::FETCH_ASSOC);        
             foreach($result as $comm){
                 echo $comm['Commentaire'],'
                 <form method="post">
