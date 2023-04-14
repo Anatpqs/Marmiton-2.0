abe5673420c4dee06a896ffdd6fad6838cb704d6
Maha1e <120331440+Maha1e@users.noreply.github.com>
2023-04-14 14:48:42 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,6 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Sportiton</title>
     <link rel="stylesheet" type="text/css" href="login.css">
   </head>
   <body>
