825072d216a6aeded343e4e84bfb1682ee514437
Maha1e <120331440+Maha1e@users.noreply.github.com>
2023-04-14 14:46:53 +0200

    Update inscription.php
    inscription after css

--- inscription.php
+++ inscription.php
@@ -1,15 +1,14 @@
 
-<!DOCTYPE html> 
-<html> 
-<head> 
-    <meta charset="utf-8"> 
-    <meta name="viewport" content="width=device-width, initial-scale=1"> 
-    <title>Hello</title> 
-<style type="text/css">
-    
-</style>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="inscription.css">
 </head> 
 <body> 
+    <div class="frame">
+      <div class="avatar"></div>
   <form method="post">
     <input type="text" name="Pseudo" id="Pseudo" placeholder="Entrez votre Pseudo" required><br>
     <input type="text" name="Login" id="Login" placeholder="Entrez votre email" required><br>
