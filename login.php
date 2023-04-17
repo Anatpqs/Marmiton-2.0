18744ebac8e88674e421408367728233cca30ea9
Andrewbcq <120384050+Andrewbcq@users.noreply.github.com>
2023-04-17 09:36:45 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,26 +5,30 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="login.css">
+    <title>Sportiton</title>
+    <link rel="stylesheet" type="text/css" href="styles/login.css">
   </head>
   <body>
-    <div class="frame">
-      <div class="avatar"></div>
-      <form method="post">
-        <input type="text" name="username" id="username" placeholder="Identifiant">
+    <div class="login-container">
+      <img src="Images/avatar.png" alt="avatar image">
+    <form method="post">
+        <label>Identifiant</label><input type="text" name="username" id="username">
         <br>
-        <input type="password" name="password" id="password" placeholder="Mot de passe">
+        <label>Mot de passe</label><input type="password" name="password" id="password">
         <br>
         <button type="submit" name="connexion">Connexion</button>
         <button type="submit" name="Accueil" >Accueil</button>
-      </form>
-    </div>
+    </form>
   </body>
 </html>
 
-
 <?php
 session_start();
+if ($_SESSION["droit"] !=-1) {
+  header("Location:accueil.php");
+}
+
+
 if(isset($_POST["connexion"])){
     $username=$_POST["username"];
     $password=$_POST["password"];
@@ -38,7 +42,7 @@ if(isset($_POST["connexion"])){
     $sql->execute(["Login"=>$username]);
     $resultat=$sql->fetch(PDO::FETCH_ASSOC);
     if($sql->rowCount()==1) {
-       if (password_verify($password,$resultat["Mdp"])){
+      if (password_verify($password,$resultat["Mdp"])){
         $_SESSION["droit"] = $resultat["Droit"];
         $_SESSION["Login"]= $resultat["Login"];
         $_SESSION["id"]= $resultat["IdUtilisateur"];
@@ -53,4 +57,6 @@ if(isset($_POST["connexion"])){
     echo "Pas d'utilisateur avec ce login";
   }
 }
+
+
 ?>
