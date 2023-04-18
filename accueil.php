a69a537a998f65e28b467b8f3df4de751adf13ad
JulianGssl <120329475+JulianGssl@users.noreply.github.com>
2023-04-18 12:43:11 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -9,11 +9,10 @@ session_start();
 if (!isset($_SESSION["droit"])) {
     $_SESSION["droit"] = -1;
 };
-
 //--------------------------------------------------------------------------------------------------------------------//
 
 // Requête SQL pour récupérer deux recettes aléatoires
-$req_sql = $db->prepare('SELECT * FROM Recette WHERE État!=0 ORDER BY RAND() LIMIT 2');
+$req_sql = $db->prepare('SELECT * FROM Recette ORDER BY RAND() LIMIT 2');
 $req_sql->execute();
 // Récupération des résultats de la requête et stockage dans des variables
 $recette1 = $req_sql->fetch();
@@ -22,10 +21,32 @@ $recette2 = $req_sql->fetch();
 //--------------------------------------------------------------------------------------------------------------------//
 
 // Requête SQl pour récupérer la recette du moment (recette ayant la meilleure moyenne)
-$req_sql_RecetteDuMoment = $db->prepare('SELECT * FROM Recette WHERE État!=0 AND Notemoy = (SELECT MAX(Notemoy) FROM Recette)');
+$req_sql_RecetteDuMoment = $db->prepare('SELECT * FROM Recette WHERE Notemoy = (SELECT MAX(Notemoy) FROM Recette)');
 $req_sql_RecetteDuMoment->execute();
 //Récupération du résultat de la requête et stockage dans la variable
 $RecetteDuMoment = $req_sql_RecetteDuMoment->fetch();
+
+//Fonction affichage de la note
+function notation($note)
+{
+  switch ($note) {
+    case 1:
+      return '<img src="Images/1etoile.png" alt="note" class="img_note">';
+      break;
+    case 2:
+      return '<img src="Images/2etoile.png" alt="note" class="img_note">';
+      break;
+    case 3:
+      return '<img src="Images/3etoile.png" alt="note" class="img_note">';
+      break;
+    case 4:
+      return '<img src="Images/4etoile.png" alt="note" class="img_note">';
+      break;
+    case 5:
+      return '<img src="Images/5etoile.png" alt="note" class="img_note">';
+      break;
+  }
+};
 
 ?>
 
@@ -44,7 +65,7 @@ $RecetteDuMoment = $req_sql_RecetteDuMoment->fetch();
 <body>
     <div id="barre">
         <hr id="bb_top">
-        <img id="logo" src="Images/cooking.png">
+        <img id="logo" src="Images/cooking.png"> <!-- // TODO Changer le logo par celui dl sur windows -->
         <form method="post" action="Recherche.php" id="formsearchbar">
             <input type="search" id="searchbar" name="searchbar" placeholder="Recette..." autocomplete="off">
         </form>
@@ -55,21 +76,21 @@ $RecetteDuMoment = $req_sql_RecetteDuMoment->fetch();
             <ul class="navbar">
                 <li class="li"><img class="icon" src="Images/userblanc.png">
                     <ul>
-                        <?php if ($_SESSION["droit"] == -1) {
+                        <?php if ($_SESSION["droit"] == -1) { // Si l'utilisateur n'est pas connecté, on affiche les liens de connexion et d'inscription
                             echo '
                   <li><a href="login.php">Se connecter</a></li> 
                   <li><a href="inscription.php">Créer un compte</a></li> ';
-                        } // Si l'utilisateur n'est pas connecté, on affiche les liens de connexion et d'inscription
+                        } 
                         ?>
-                        <?php if ($_SESSION["droit"] == 0 || $_SESSION["droit"] == -2 ) {
+                        <?php if ($_SESSION["droit"] == 0) { // Si l'utilisateur est connecté, on affiche les liens de profil et de déconnexion
                             echo '
                   <li><a href="profil.php">Mon Profil</a></li> 
                   <li><a href="deconnexion.php">Déconnexion</a></li> ';
-                        } // Si l'utilisateur est connecté, on affiche les liens de profil et de déconnexion
+                        } 
                         ?>
-                        <?php if ($_SESSION["droit"] == 1) {
+                        <?php if ($_SESSION["droit"] == 1) { //Admin
                             echo '<li><a href="admin.php">Admin</a></li>
-                            <li><a href="deconnexion.php">Déconnexion</a></li>'; //Admin
+                            <li><a href="deconnexion.php">Déconnexion</a></li>'; 
                         }
                         ?>
                     </ul>
@@ -99,24 +120,71 @@ $RecetteDuMoment = $req_sql_RecetteDuMoment->fetch();
                 <form id="myForm" action="recette.php" method="post">
                     <input type="hidden" name="idRecette" value="<?php echo $recette1["IdRecette"]?>">
                 </form>
-                <a href="#" onclick="document.getElementById('myForm').submit();"><img
-                        src=" Images/Recette/<?php echo $recette1['IdRecette']; ?>.jpg"> </a>
-
-
-                <p><?php echo $recette1['Notemoy']; ?></p>
+                <a href="#" onclick="document.getElementById('myForm').submit();"><img class="image" src=" Images/<?php echo $recette1['IdRecette']; ?>.jpg"></a>
+                <?php
+                //Si la recette vient d'être créé pas de note
+                if ($recette1["Notemoy"] !== NULL) {
+                echo notation($recette1["Notemoy"]);
+                }
+                ?>
             </div>
 
             <div class="recette_du_jour affichage">
+            
+                <?php
+                    // Récupération de l'ID de l'utilisateur connecté.
+                    $login = $_SESSION['Login'];
+                    $reqLog = $db->prepare("SELECT Pseudo,IdUtilisateur FROM Utilisateur WHERE Login = :Login");
+                    $reqLog->execute(['Login' => $login]);
+                    $resultat = $reqLog->fetch(PDO::FETCH_ASSOC);
+                    // Stockage de l'ID de l'utilisateur connecté.
+                    $user_id = $resultat['IdUtilisateur'];
+                    
+                    // Récupération des recettes favorites de l'utilisateur.
+                    $req_fav = $db->prepare("SELECT Recette_pref.Id_recette
+                    FROM Recette_pref
+                    WHERE Recette_pref.Id_utilisateur = :user_id");
+                    $req_fav->execute(['user_id' => $user_id]);
+
+                    // Vérification si l'utilisateur a des recettes favorites.
+                    if ($req_fav->rowCount() > 0) {
+                        // Récupération des tags des recettes favorites de l'utilisateur.
+                        $req_tags = $db->prepare("SELECT DISTINCT Tag.Mot_clé
+                            FROM Tag
+                            JOIN Recette ON Tag.Recette_assoc = Recette.IdRecette
+                            JOIN Recette_pref ON Recette.IdRecette = Recette_pref.Id_recette
+                            WHERE Recette_pref.Id_utilisateur = :user_id
+                            AND Recette_pref.Id_recette NOT IN (
+                                SELECT Id_recette FROM Recette_pref WHERE Id_utilisateur = :user_id
+                            )");
+                        $req_tags->execute(['user_id' => $user_id]);
+                        $resultat_tags = $req_tags->fetchAll(PDO::FETCH_ASSOC);
+
+                        // Affichage des tags des recettes favorites de l'utilisateur. 
+                        // ! PROBLEME A PARTIR D'ICI 
+                        foreach ($resultat_tags as $tag) {
+                        echo $tag['Mot_clé'] . "<br>";
+                        }
+                    }else {
+                    echo "L'utilisateur n'a pas encore ajouté de recettes à ses favoris.";
+                    }
+                ?>            
+
+
+
                 <!-- Affichage de la deuxième recette du jour -->
                 <h3><?php echo $recette2['Nom']; ?></h3>
-                <!-- Lien vers la recette -->
+                <!-- Lien vers la recette -->   
                 <form id="myForm2" action="recette.php" method="post">
                     <input type="hidden" name="idRecette" value="<?php echo $recette2["IdRecette"]?>">
                 </form>
-                <a href="#" onclick="document.getElementById('myForm2').submit();"><img
-                        src="Images/Recette/<?php echo $recette2['IdRecette']; ?>.jpg"> </a>
-
-                <p><?php echo $recette2['Notemoy']; ?></p>
+                <a href="#" onclick="document.getElementById('myForm2').submit();"><img class="image" src="Images/<?php echo $recette2['IdRecette']; ?>.jpg"> </a>
+                <?php
+                //Si la recette vient d'être créé pas de note
+                if ($recette2["Notemoy"] !== NULL) {
+                echo notation($recette2["Notemoy"]);
+                }
+                ?>
             </div>
         </div>
 
@@ -129,9 +197,7 @@ $RecetteDuMoment = $req_sql_RecetteDuMoment->fetch();
                 <form id="myForm3" action="recette.php" method="post">
                     <input type="hidden" name="idRecette" value="<?php echo $RecetteDuMoment["IdRecette"]?>">
                 </form>
-                <a href="#" onclick="document.getElementById('myForm3').submit();"><img class="image"
-                        src="Images/Recette/<?php echo $RecetteDuMoment['IdRecette']; ?>.jpg"> </a>
-
+                <a class="imageMoment" href="#" onclick="document.getElementById('myForm3').submit();"><img class="image" src="Images/<?php echo $RecetteDuMoment['IdRecette']; ?>.jpg"> </a>
 
                 <p><?php echo $RecetteDuMoment['Description']; ?></p>
             </div>
