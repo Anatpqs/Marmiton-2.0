c3ec34a81373ab2460713e788bc4f1c5e98006db
Anatpqs <120056597+Anatpqs@users.noreply.github.com>
2023-04-18 16:05:04 +0200

    Update modif_recette.php

--- modif_recette.php
+++ modif_recette.php
@@ -25,42 +25,98 @@ $IdRecette=$_COOKIE["IdRecette"];
 <head>
 <meta charset="utf-8"/>
 <title>Modification recette</title>
-
+<link rel="stylesheet" href="styles/modif_recette.css" />
 
 <script>
-    function ajouter(){
-        var div = document.createElement('div');
-        div.innerHTML = '<label>Nom :</label><input type="text" name="nv_nom_ing[]" class="nom_ing" required><br><label>Quantité :</label><input type="number" name="nv_quantite[]" class="quantite" min="0" step="0.01" required><br><label>Unité :</label><input type="text" name="nv_unite[]" class="unite"><br><label>Prix :</label><input type="number" name="nv_prix[]" class="prix" min="0" step="0.01" required><br>';
-        document.getElementById('ing').appendChild(div);
-    }
+   function ajouter(){
+          var div = document.createElement('div');
+          div.innerHTML = '<div class="TextBloc ingredients"> <input type="text" name="nom_ing[]" class="InputText ingredients" required><span class="labelText ingredients">Nom: </span> </div> <div class="TextBloc ingredients"> <input type="number" name="quantite[]" class="InputText ingredients" min="0" step="0.01" required><label class="labelText ingredients">Quantité: </label> </div> <div class="TextBloc ingredients"> <label class="labelText ingredients" style="top:-15px">Unité: </label> <select id="unite[]" name="unite[]" class="InputText ingredients"> <option value="">Aucune</option> <option value="kg">kg</option> <option value="l">l</option> </select> </div>'
+          document.getElementById('nv_ing').appendChild(div);
+      }
 
-    function insertDatalist() {
-            var div = document.createElement('div');
-          
-            div.innerHTML = '<input list="tags" name="nv_tag[]">\n<datalist id="tags">\n';
-            
-            <?php
-              $sql4 = $db->prepare("SELECT * FROM Tag;");
-              $sql4->execute([]);
-              $resultat4 = $sql4->fetchAll();
-              foreach ($resultat4 as $tag) {
-                echo 'div.innerHTML += \'<option value="' . $tag["Mot_clé"] . '">\';' . "\n";
-              }
-            ?>
-            
-            div.innerHTML += '</datalist>';
-      
-            document.getElementById('tag_div').appendChild(div);
+      function insertDatalist() {
+        var div = document.createElement('div');
+        div.setAttribute('class', 'tagDiv');
+
+        div.innerHTML = '<input class="InputText tag" list="tags" name="tag[]" required/><datalist id="tags">' +
+        <?php
+          $sql4 = $db->prepare("SELECT * FROM Tag;");
+          $sql4->execute([]);
+          $resultat4 = $sql4->fetchAll();
+          $options = '';
+          foreach ($resultat4 as $tag) {
+            $options .= '<option value="' . $tag["Mot_clé"] . '">';
           }
+        ?>     
+
+        document.getElementById('tag_div').appendChild(div);
+      }
+
+      function removeDatalist(){
+        var tag_div = document.getElementById('tag_div');
+        var tagDivs = tag_div.getElementsByClassName('tagDiv');
+        if (tagDivs.length > 0) {
+        tag_div.removeChild(tagDivs[tagDivs.length - 1]);
+        }
+      }
+
+
+      const tx = document.getElementsByTagName("textarea");
+      for (let i = 0; i < tx.length; i++) {
+        tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+        tx[i].addEventListener("input", OnInput, false);
+      }
+      function OnInput() {
+        this.style.height = 0;
+        this.style.height = (this.scrollHeight) + "px";
+      }
+      var loadFile = function(event) {
+        var imagefile = document.getElementById('imagefile');
+        imagefile.src = URL.createObjectURL(event.target.files[0]);
+        imagefile.onload = function() {
+          URL.revokeObjectURL(imagefile.src) // free memory
+        }
+      };
 
     </script>
 
 </head>
 
+<header>
+      <div id="logo_div"><a href="accueil.php"><img id="logo" src="Images/cooking.png" alt="logo"></a></div>
+      <div id="titre"><?php echo '<h1>Sportiton</h1>' ?></div>
+      <div class="profil">
+          <ul class="navbar">
+              <li class="li"><img class="icon" src="Images/userblanc.png">
+                  <ul>
+                      <?php if ($_SESSION["droit"] == -1) {
+                          echo '
+                        <li><a href="login.php">Se connecter</a></li> 
+                        <li><a href="inscription.php">Créer un compte</a></li> ';
+                      } // Si l'utilisateur n'est pas connecté, on affiche les liens de connexion et d'inscription
+                      ?>
+                      <?php if ($_SESSION["droit"] == 0) {
+                          echo '
+                              <li><a href="profil.php">Mon Profil</a></li> 
+                              <li><a href="deconnexion.php">Déconnexion</a></li> ';
+                      } // Si l'utilisateur est connecté, on affiche les liens de profil et de déconnexion
+                      ?>
+                      <?php if ($_SESSION["droit"] == 1) {
+                          echo '<li><a href="admin.php">Admin</a></li>
+                          <li><a href="deconnexion.php">Déconnexion</a></li>'; //Admin
+                      }
+                      ?>
+                  </ul>
+              </li>
+          </ul>
+      </div>
+    </header>
+
 <body>
-
+  
+<div id="main">
 <h1>Modifier la recette</h1>
-
+<br>
 <?php 
 
 $sql=$db->prepare("SELECT * FROM Recette WHERE IdRecette=?;");
@@ -70,10 +126,13 @@ $resultat=$sql->fetch(PDO::FETCH_ASSOC);
 echo '
     <form method="post" enctype="multipart/form-data">
     <input type="hidden" name="IdRecette" value="'.$IdRecette.'">
-        <label for="titre">Titre de la recette :</label>
-        <input type="text" name="titre" id="titre" value="'.$resultat["Nom"].'" required><br><br>
 
-        <label for="tag">Mots-clés:</label>';
+      <div class="TextBloc">
+        <input type="text" name="titre" class="InputText" value="'.$resultat["Nom"].'" required><span class="labelText">Titre de votre recette:</span> 
+      </div>
+
+      <div class="TextBloc tag">
+      <span for="tag" class="labelText" >Choisir des mots-clés:</span><br>';
         //Affichage mot clé 
         $sql5=$db->prepare("SELECT * FROM Tag WHERE Recette_assoc=?;");
         $sql5->execute([$IdRecette]);
@@ -81,7 +140,7 @@ echo '
         foreach ($resultat5 as $tag)
         {
           echo '
-          <input list="tags" id="tag" name="tag[]" value='.$tag["Mot_clé"].'>
+          <input list="tags" class="InputText" id="tag" name="tag[]" value='.$tag["Mot_clé"].' required>
           <input type="hidden" name="id_tag[]" value='.$tag["IdTag"].'>
           <datalist id="tags">';
           
@@ -93,26 +152,37 @@ echo '
               echo '<option value='.$tags["Mot_clé"].'>';
       
             };
-          echo '</datalist><br><br>';
+          echo '</datalist>';
         }
         //Ajout nv_tags
         echo '
-        <button type="button" onclick="insertDatalist()">Ajouter des tags</button><br><br>
-        <div id="tag_div">
+        <button type="button" onclick="insertDatalist()">Ajouter des tags</button>
+        <button type="button" onclick="removeDatalist()">Enlever des tags</button> 
         </div>
 
-        <label for="Nb_personne">Nombre de personne :</label>
-        <input type="number" name="Nb_personne" id="Nb_personne" min="1" value='.$resultat["Nb_personne"].' required><br><br>
+        <div id="tag_div" style="display:inline-block"> </div>
 
-        <label for="Temps_prep">Temps de préparation (en min) :</label>
-        <input type="number" name="Temps_prep" id="Temps_prep" min="1" value='.$resultat["Temps_prep"].' required><br><br>
+        <div class="TextBloc">
+          <label class="labelText description">Description :</label><br>
+          <textarea name="Description" id="Description" class="InputText description" required>'.$resultat["Description"].'</textarea>
+        </div>
 
-        <label for="Temps_cuis">Temps de cuisson (en min) :</label>
-        <input type="number" name="Temps_cuis" id="Temps_cuis" min="0" value='.$resultat["Temps_cuis"].' required><br><br>
-        
+        <div class="TextBloc">
+          <input type="number" name="Nb_personne" class="InputText" min="1" value='.$resultat["Nb_personne"].' required><span class="labelText">Nombre de personne :</span>   
+        </div>
+
+        <div class="TextBloc Temps_prep">  
+          <input type="number" name="Temps_prep" class="InputText" min="1" value='.$resultat["Temps_prep"].' required><span class="labelText">Temps de préparation (en min) :</span>           
+        </div>
+
+        <div class="TextBloc Temps_cuis">   
+          <input type="number" name="Temps_cuis" class="InputText" min="0" value='.$resultat["Temps_cuis"].' required><span class="labelText">Temps de cuisson (en min) :</span>
+        </div>
+
         <!-- Ingrédient -->
-        <div id="ing">
-        <label for="Ing">Ingrédients:</label><br>
+        <label id="Ing" style="display:block">Ingrédients:</label>
+        <div id="blocingredients">
+        
         ';
 
         $sql2=$db->prepare("SELECT * FROM Ingrédient WHERE Recette=?");
@@ -122,37 +192,54 @@ echo '
         {
         
         echo '
-        
-            <label>Nom :</label><input type="text" name="nom_ing[]" class="nom_ing" value="'.$ing["Nom"].'" required><br>
-            <label>Quantité :</label><input type="number" name="quantite[]" class="quantite" min="0" step="0.01" value="'.$ing["Quantité"].'" required><br>
-            <label>Unité :</label><input type="text" name="unite[]" class="unite" value="'.$ing["Unité"].'" ><br>
-            <label>Prix :</label><input type="number" name="prix[]" class="prix" min="0" step="0.01" value="'.$ing["Prix"].'" required>
-            <input type="hidden" name="id[]" value="'.$ing["IdIngrédient"].'"><br>
-            <br><br>';
+            <div class="TextBloc ingredients">
+              <input type="text" name="nom_ing[]" class="InputText ingredients" value="'.$ing["Nom"].'" required><span class="labelText ingredients">Nom: </span>
+            </div>
+
+            <div class="TextBloc ingredients">
+              <input type="number" name="quantite[]" class="InputText ingredients" min="0" step="0.01" value="'.$ing["Quantité"].'" required><label class="labelText ingredients">Quantité: </label>
+            </div>
+
+            <div class="TextBloc ingredients">
+              <label class="labelText ingredients" style="top:-15px">Unité: </label>
+              <select id="unite[]" name="unite[]" class="InputText ingredients">
+                    <option value="" '.(($ing["Unité"] === "") ? "selected" : "").'>Aucune</option> 
+                    <option value="kg" '.(($ing["Unité"] === "kg") ? "selected" : "").'>kg</option>                    
+                    <option value="l" '.(($ing["Unité"] === "l") ? "selected" : "").'>l</option>
+              </select>                  
+            </div>
+
+            <input type="hidden" name="id[]" value="'.$ing["IdIngrédient"].'">
+            ';
 
         };
 
         echo '
+        <div id="nv_ing"></div>
+        <button type="button" style="float:right; margin-top: 1em;" onclick="ajouter()">Ajouter un ingrédient</button>
         </div>
-        <button type="button" onclick="ajouter()">Ajouter un ingrédient</button>
-        <br><br> 
+        <br><br>
 
-        <label for="Description">Description :</label><br>
-        <textarea name="Description" id="Description" required>'.$resultat["Description"].'</textarea><br><br>
-        
-        <label for="Instruction">Instruction :</label><br>
-        <textarea name="Instruction" id="Instruction" required>'.$resultat["Instruction"].'</textarea><br><br>
-
+        <div class="TextBloc">
+          <label for="Instruction" class ="labelText description">Instruction :</label><br>
+          <textarea name="Instruction" id="Instruction" class="InputText description"  required>'.$resultat["Instruction"].'</textarea>
+        </div>
       
-        <label for="file">Image de la recette:</label>
-        <img src="Images/Recette/'.$IdRecette.'.jpg" alt="recette" width="100px" height="100px" >
-        <input type="file" id="file" name="file"> 
-        <br>
-        <input type="submit" name="submit" value="Modifier la recette">
+        
+        <div class="image-upload">
+              <label for="file-input">
+                <img id="imagefile" src="Images/Recette/'.$IdRecette.'.jpg" style="margin-top: 0px; border: solid 2px black; border-radius: 50%"/>
+              </label>
+              <input type="file" id="file-input" name="file" onchange="loadFile(event)"/>
+            </div>
+            <input type="submit" name="submit" value="Modifier la recette" style="float:right; margin-top: -1em">
+        </div>
     </form>
-';
+'; ?>
 
+</div>
 
+<?php
 // Récupération des données du formulaire
 if (isset($_POST['submit'])) {
     $Nom = strval($_POST['titre']); 
@@ -258,5 +345,7 @@ echo "<meta http-equiv='refresh' content='0'>";
 
 ?>
 </body>
+
+<footer></footer>
 
 </html>
