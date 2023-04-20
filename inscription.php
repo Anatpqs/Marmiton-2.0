d0e7eb9dc11f046bdc8cfde54fb96730b610f9c5
Anatpqs <120056597+Anatpqs@users.noreply.github.com>
2023-04-20 12:57:10 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -4,7 +4,7 @@
 <head> 
     <meta charset="utf-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
-    <title>Hello</title> 
+    <title>Sportiton</title> 
 <style type="text/css">
     
 </style>
